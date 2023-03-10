44b8395b8dcec475dd39d733eb3a30580ba85176
Horashio <horashi0@users.noreply.github.com>
2023-03-10 18:06:42 +0000

    Two ways to make a function

---
+++ Learning-Js/project.js
@@ -0,0 +1,17 @@
+// I am following the tutorial: https://www.youtube.com/watch?v=E3XxeE7NF30&t=539s
+// 1. Deposit money
+// 2. Determine number of lines to bet on
+// 3. Collect bet amount
+// 4. Spin slot machine
+// 5. Check if user won
+// 6. Give user winnings if they won. If not take their bet
+// 7. Play again
+
+function deposit() {
+    return 1
+}
+const deposit_learn = () => {
+
+}
+
+deposit()
